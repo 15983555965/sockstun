89ba3f3bfb580d4554119e82dc32bb134097d4d2
ssl <314462650@qq.com>
2025-05-07 16:59:29 +0800

    提交第2次

--- app/src/main/java/hev/sockstun/MainActivity.java
+++ app/src/main/java/hev/sockstun/MainActivity.java
@@ -160,7 +160,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         //updateUI();
         timerTextView.setVisibility(View.VISIBLE);
         timerTitle.setVisibility(View.VISIBLE);
-        timerTextView.setVisibility(View.VISIBLE);
+        timerTextView.setVisibility(View.GONE);
     }
 
     @Override
